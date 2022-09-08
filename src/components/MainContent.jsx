067602b93ffa869fd35696280558e170011c2b59
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-09-09 02:03:21 +0530

    add: slogan maker

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const MainContent = () => {
+  return (
+    <div className="flex flex-col gap-12 absolute rounded-lg top-32 bg-white mx-40 px-48 py-16 text-black12">
+      <div className="flex flex-col gap-4">
+        <h3 className=" text-4xl font-bold ">Free slogan maker</h3>
+        <p className="text-black30 text-xl">
+          Simply enter a term that describes your business, and get up to 1,000
+          relevant slogans for free.
+        </p>
+      </div>
+      <div>
+        <label className="flex flex-col gap-2 text-black30 font-medium ">
+          Word for your slogan
+          <div className="flex items-center w-[22rem] px-3 py-4 border border-black85 rounded-lg">
+            <input type="text" className=" outline-none" value="Cozy" />
+            <span className="ml-auto">
+              <img src="/icons/Union.png" alt="cross_icon" />
+            </span>
+          </div>
+        </label>
+      </div>
+      <button className=" flex w-52 justify-center py-3 px-6 bg-primaryBlue h-12 rounded font-medium text-white">
+        Generate slogans
+      </button>
+      <hr className="bg-black85" />
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
       colors: {
@@ -10,6 +10,12 @@ module.exports = {
         black30: "#4D4D4D",
         black98: "#FAFAFA",
         black95: "#F2F2F2",
+        black85: "#D9D9D9",
+      },
+      fontFamily: {
+        galanoReg: "galanoReg",
+        galanoMed: "galanoMed",
+        galanoSB: "galanoSB",
       },
     },
   },
