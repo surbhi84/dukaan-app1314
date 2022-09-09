89882157fcc3209efee105d6daf53e89a97d74ad
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-09-09 18:45:14 +0530

    fix: alignments

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AboutContent } from "./AboutContent";
+// import { AboutContent } from "./AboutContent";
 import { Pagination } from "./Pagination";
 import { SloganResults } from "./SloganResults";
 
--- src/components/SloganResults.jsx
+++ src/components/SloganResults.jsx
@@ -3,7 +3,15 @@ import { staticData } from "../staticData";
 
 export const SloganResults = () => {
   return (
-    <div>
+    <div className="relative">
+      <div className="flex flex-col gap-14 absolute -left-32 top-80">
+        <div className="bg-black30 px-2 py-1 w-fit rounded text-white">
+          Click to copy
+        </div>
+        <div className="bg-black30 px-2 py-1 w-fit rounded text-white">
+          Copied!
+        </div>
+      </div>
       <div className="flex justify-between items-center pb-9">
         <h5 className="text-xl text-black12 font-semibold">
           We have generated 1,023 slogans for “cozy”
@@ -12,7 +20,7 @@ export const SloganResults = () => {
           Download all
         </button>
       </div>
-      <div className="flex flex-wrap gap-5 text-black12">
+      <div className="flex flex-wrap justify-center gap-5 text-black12">
         {staticData.map((data) => (
           <p className="py-2 px-4 bg-black95 w-[22rem]">{data}</p>
         ))}
